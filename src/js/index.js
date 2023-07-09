23bfc33a9b62ab3312ea7b308bd83119d2c8f649
Kei3108 <128880307+Kei3108@users.noreply.github.com>
2023-07-09 23:24:51 +0000

    Finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from 'prop-types';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -9,4 +10,65 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const counterStyle = {
+    background: "black",
+}
+
+const iconStyle = {
+    fontSize: "40px",
+    color: "white",
+    marginTop: "15px",
+    textAling: "center"
+}
+
+const numberStyle = {
+    fontSize: "50px",
+    color: "white",
+    textAling: "center"
+}
+
+function Counter(props) {
+    return (
+        <div className="container mt-4 rounded" style={counterStyle}>
+            <div className="row">
+                <div className="col rounded">
+                    <i className="fas fa-clock" style={iconStyle}>
+                    </i>
+                </div>
+                <div className="col rounded" style={numberStyle}>{props.Sixth % 10}</div> 
+                <div className="col rounded" style={numberStyle}>{props.Fifth % 10}</div> 
+                <div className="col rounded" style={numberStyle}>{props.Fourth % 10}</div> 
+                <div className="col rounded" style={numberStyle}>{props.Third % 10}</div> 
+                <div className="col rounded" style={numberStyle}>{props.Second % 10}</div> 
+                <div className="col rounded" style={numberStyle}>{props.First % 10}</div> 
+            </div> 
+        </div>          
+
+    )
+}
+
+let time = 0;
+
+setInterval(function() {
+    let sixth = Math.floor(time / 100000);
+    let fifth = Math.floor(time / 10000);
+    let fourth = Math.floor(time / 1000);
+    let third = Math.floor(time / 100); 
+    let second = Math.floor(time / 10);
+    let first = Math.floor(time / 1); 
+    time++,
+
+    ReactDOM.render(<Counter Sixth={sixth} Fifth={fifth} Fourth={fourth} Third={third} Second={second} First={first}/>, document.querySelector("#app"));
+}, 1000)
+
+Counter.propTypes ={ 
+    Sixth: propTypes.number,
+    Fifth: propTypes.number,
+    Fourth: propTypes.number,
+    Third: propTypes.number,
+    Second: propTypes.number,
+    First: propTypes.number,
+}
+
+
